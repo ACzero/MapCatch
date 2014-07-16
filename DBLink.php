22bba67c61bd50fa3dc0e036688cdb78b563116f
ACzero <282493754@qq.com>
2014-07-16 17:12:19 +0800

    db

--- DBLink.php
+++ DBLink.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect('localhost:3306','zhanhui','zhanhui@)!$');
+$link = mysql_connect('127.0.0.1:3306','zhanhui','zhanhui@)!$');
 mysql_select_db("mapdata",$link);
 mysql_query("SET NAMES UTF8");
 function DBclose()
